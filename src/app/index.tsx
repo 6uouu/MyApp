a9c512bcfdbdc88ba42480b25fb62e519e333b12
6uouu <mongsamc@gmail.com>
2024-06-10 17:58:43 +0900

    Insert expo-route

---
+++ src/app/index.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+
+const Index = (): JSX.Element => {
+    return (
+        <View>
+            <Text>
+                Hellow Minji!
+            </Text>
+        </View>
+    )
+}
+
+export default Index
